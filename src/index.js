0cbcaa0ecee43cdff1162c24c17f3303c66e45c1
ynkrpnk <ankarpenko35@gmail.com>
2026-05-09 23:34:05 +0300

    add BOOKS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-'use strict';
+"use strict";
+
+const BOOKS = {
+  fantazy: [
+    {
+      id: 1,
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      year: 1954,
+    },
+    {
+      id: 2,
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      year: 1937,
+    },
+    {
+      id: 3,
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      year: 1997,
+    },
+    {
+      id: 4,
+      title: "A Game of Thrones",
+      author: "George R.R. Martin",
+      year: 1996,
+    },
+  ],
+  scienceFiction: [
+    {
+      id: 1,
+      title: "Dune",
+      author: "Frank Herbert",
+      year: 1965,
+    },
+    {
+      id: 2,
+      title: "Neuromancer",
+      author: "William Gibson",
+      year: 1984,
+    },
+    {
+      id: 3,
+      title: "Foundation",
+      author: "Isaac Asimov",
+      year: 1951,
+    },
+    {
+      id: 4,
+      title: "1984",
+      author: "George Orwell",
+      year: 1948,
+    },
+  ],
+};
+
+
+// за допомогою деструктуризації отримати третю книгу з масиву fantazy і зберегти її у змінну. Також зберегти у змінну ім’я автора четвертої книги з масиву scienceFiction.
+// Назви змінних: для книги — thirdBook, для автора — writer.
+// Потім по черзі вивести ці змінні в консоль для перевірки.
+// * Завдання підвищеної складності: реалізувати отримання даних в один рядок.
+
+function book ({})
